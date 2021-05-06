fe71b20cdb1842c1ec3a2792c567f9c103120d00
Karthik-avj <ka.avj99@gmail.com>
2021-05-07 00:10:19 +0530

    Add files via upload

--- nearest_intersection_object.c
+++ nearest_intersection_object.c
@@ -1,14 +1,14 @@
-void nearest_intersection_object(float *objects, float *origin, float *ray_direction, int *objects_len, float *min_dist, int *object_idx){
-	float distances[objects_len];
-	for(int i=0; i<objects_len; i++){
-		float *center = {objects[i][0], objects[i][1], objects[i][2]};
-		sphere_intersection(origin, ray_direction, center, objects[i][3], distances[i]);	
+void nearest_intersection_object(float *objects, float *origin, int *object_n, float *ray_direction, int *objects_len, float *min_dist, int *object_idx){
+	float distances[*objects_len];
+	for(int i=0; i<(*objects_len); i++){
+		float *center = {objects[i*(*object_n)+0], objects[i*(*object_n)+1], objects[i*(*object_n)+2]};
+		sphere_intersection(origin, ray_direction, center, objects[i*(*object_n)+3], &distances[i]);	
 	}
 
-	for(int i=0; i<objects_len; i++){
-		if((distances[i]<min_dist) && (distances[i]>0)){
-			min_dist = distances[i];
-			object_idx = i;
+	for(int i=0; i<(*objects_len); i++){
+		if((distances[i]<(*min_dist)) && (distances[i]>0)){
+			*min_dist = distances[i];
+			*object_idx = i;
 		}
 	}
 }
